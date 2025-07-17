5186346728da6b848252801ada89f42b390937c2
Mostafa6151 <mosollini@Mosolinnis-4.local>
2025-07-18 00:16:42 +0330

    create project

---
+++ lib/ai/providers.ts
@@ -0,0 +1,38 @@
+import {
+  customProvider,
+  extractReasoningMiddleware,
+  wrapLanguageModel,
+} from 'ai';
+import { openai } from '@ai-sdk/openai';
+import { isTestEnvironment } from '../constants';
+import {
+  artifactModel,
+  chatModel,
+  reasoningModel,
+  titleModel,
+} from './models.test';
+
+export const myProvider = isTestEnvironment
+    ? customProvider({
+      languageModels: {
+        'chat-model': chatModel,
+        'chat-model-reasoning': reasoningModel,
+        'title-model': titleModel,
+        'artifact-model': artifactModel,
+      },
+    })
+    : customProvider({
+      languageModels: {
+        'chat-model': openai('gpt-4'),
+        'chat-model-reasoning': wrapLanguageModel({
+          model: openai('gpt-4'),
+          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        }),
+        'title-model': openai('gpt-3.5-turbo'),
+        'artifact-model': openai('gpt-3.5-turbo'),
+      },
+      imageModels: {
+        // فقط در صورت نیاز اضافه شود
+        'small-model': openai.imageModel('dall-e-3'),
+      },
+    });
