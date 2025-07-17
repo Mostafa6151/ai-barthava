6157c44426c6b3fe8e899cade0203ce443f0e087
Mostafa6151 <mosollini@Mosolinnis-4.local>
2025-07-18 03:19:34 +0330

    grok-2-vision-1212

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -3,17 +3,18 @@ import {
   extractReasoningMiddleware,
   wrapLanguageModel,
 } from 'ai';
-import { openai } from '@ai-sdk/openai';
-import { isTestEnvironment } from '../constants';
+import { xai } from '@ai-sdk/xai';
 import {
   artifactModel,
   chatModel,
   reasoningModel,
   titleModel,
 } from './models.test';
+import { isTestEnvironment } from '../constants';
+import {createOpenAI, openai} from '@ai-sdk/openai'
 
 export const myProvider = isTestEnvironment
-    ? customProvider({
+  ? customProvider({
       languageModels: {
         'chat-model': chatModel,
         'chat-model-reasoning': reasoningModel,
@@ -21,18 +22,43 @@ export const myProvider = isTestEnvironment
         'artifact-model': artifactModel,
       },
     })
-    : customProvider({
+  : customProvider({
       languageModels: {
-        'chat-model': openai('gpt-4-0613'),
+        'chat-model': xai('grok-2-vision-1212'),
         'chat-model-reasoning': wrapLanguageModel({
-          model: openai('gpt-4-0613'),
+          model: xai('grok-3-mini-beta'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-        'title-model': openai('gpt-3.5-turbo'),
-        'artifact-model': openai('gpt-3.5-turbo'),
+        'title-model': xai('grok-2-1212'),
+        'artifact-model': xai('grok-2-1212'),
       },
       imageModels: {
-        // فقط در صورت نیاز اضافه شود
-        'small-model': openai.imageModel('dall-e-3'),
+        'small-model': xai.imageModel('grok-2-image'),
       },
     });
+
+
+// export const myProvider = isTestEnvironment
+//     ? customProvider({
+//       languageModels: {
+//         'chat-model': chatModel,
+//         'chat-model-reasoning': reasoningModel,
+//         'title-model': titleModel,
+//         'artifact-model': artifactModel,
+//       },
+//     })
+//     : customProvider({
+//       languageModels: {
+//         'chat-model': openai('gpt-4-0613'),
+//         'chat-model-reasoning': wrapLanguageModel({
+//           model: openai('gpt-4-0613'),
+//           middleware: extractReasoningMiddleware({ tagName: 'think' }),
+//         }),
+//         'title-model': openai('gpt-3.5-turbo'),
+//         'artifact-model': openai('gpt-3.5-turbo'),
+//       },
+//       imageModels: {
+//         // فقط در صورت نیاز اضافه شود
+//         'small-model': openai.imageModel('dall-e-3'),
+//       },
+//     });
